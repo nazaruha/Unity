240cbee7f6e2c40bf4247e6e71e94b61a3fe76e9
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-11-23 22:21:18 +0100

    Save HighScore value in a PlayerPrefabs local project storage

--- ApplePicker/Assets/Scripts/HighScore.cs
+++ ApplePicker/Assets/Scripts/HighScore.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,10 +8,15 @@ public class HighScore : MonoBehaviour
 {
     static public int score = 1000;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake() // вызываетс€ при создании экземпл€ра класса HighScore(то есть Awake() всегда вызываетс€ перед Start()).
     {
-        
+        // якщо значенн€ HighScore вже ≥снуЇ в PlayerPrefs - прочитати його. PlayerPrefs Ч это словарь значений, на которые можно ссылатьс€ по ключам(то есть уникальным строкам)
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        // «берегети найкращий результат HighScore в хранилищ≥
+        PlayerPrefs.SetInt("HighScore", score);
     }
 
     // Update is called once per frame
@@ -18,5 +24,10 @@ public class HighScore : MonoBehaviour
     {
         Text gt = this.GetComponent<Text>();
         gt.text = $"High Score: ${score}";
+        // ќбновити HighScore в PlayerPrefs, €кщо необх≥дно
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
     }
 }
